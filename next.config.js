c8126bf9746ca7c64a137aa46855ba35d3083d92
Chris Rittelmeyer <github.10.iamchris@spamgourmet.com>
2022-01-15 18:54:13 -0500

    feat(pwa): more pwa updates

--- next.config.js
+++ next.config.js
@@ -1,37 +1,27 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-const withPWA = require('next-pwa')
+const withPlugins = require('next-compose-plugins')
+const withPwa = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 const { withSentryConfig } = require('@sentry/nextjs')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-const withBundleAnalyzerExport = withBundleAnalyzer({})
-const withPWAExport = withPWA(withBundleAnalyzerExport)
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-
-  pwa: {
-    dest: 'public',
-    // disable: process.env.NODE_ENV === 'development'
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest\.json$/]
-  }
-}
-
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(withPWAExport, sentryWebpackPluginOptions)
+module.exports = withSentryConfig(
+  withPlugins([
+    withBundleAnalyzer,
+    [
+      withPwa,
+      {
+        pwa: {
+          dest: 'public',
+          disable: process.env.NODE_ENV === 'development',
+          runtimeCaching,
+          buildExcludes: [/middleware-manifest\.json$/]
+        }
+      }
+    ]
+  ]),
+  { silent: true }
+)
