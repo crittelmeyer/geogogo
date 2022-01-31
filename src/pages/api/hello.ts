d8da5979ab19dd4bcbe06fa3f1db71e6cdef88a5
Chris Rittelmeyer <github.10.iamchris@spamgourmet.com>
2022-01-30 20:25:47 -0500

    feat(perf): remove lingering sentry stuff

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { withSentry } from '@sentry/nextjs'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' })
 }
 
-export default withSentry(handler)
+export default handler
